111c86d212f0f837f6c1dace4e7a326ba8a1925c
siavash.shojaei <bro.shojaei@gmail.com>
2023-03-26 15:06:09 +0330

    #3 add main function with infinit loop (REPL)

--- cmd.c
+++ cmd.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct{
+typedef struct {
     char* buffer;
     size_t buffer_length;
     size_t input_length;
-}InputBuffer;
+} InputBuffer;
 
 InputBuffer* new_input_buffer()
 {
@@ -14,4 +17,19 @@ InputBuffer* new_input_buffer()
   input_buffer->input_length = 0;
 
   return input_buffer;
+}
+int main(int argc, char* argv[])
+{
+  InputBuffer* input_buffer = new_input_buffer();
+  while (true) {
+    print_prompt();
+    read_input(input_buffer);
+
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+      close_input_buffer(input_buffer);
+      exit(EXIT_SUCCESS);
+    } else {
+      printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+    }
+  }
 }
