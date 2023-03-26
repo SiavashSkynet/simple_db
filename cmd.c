a75622cd32e4aef326f5c55792c0263ff33700a4
siavash.shojaei <bro.shojaei@gmail.com>
2023-03-26 15:10:08 +0330

    #3 add read & close and print functions

--- cmd.c
+++ cmd.c
@@ -9,15 +9,12 @@ typedef struct {
     size_t input_length;
 } InputBuffer;
 
-InputBuffer* new_input_buffer()
-{
-  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-  input_buffer->buffer = NULL;
-  input_buffer->buffer_length = 0;
-  input_buffer->input_length = 0;
+// function declaration:
+InputBuffer* new_input_buffer();
+void print_prompt();
+void read_input(InputBuffer* inp);
+void close_input_buffer();
 
-  return input_buffer;
-}
 int main(int argc, char* argv[])
 {
   InputBuffer* input_buffer = new_input_buffer();
@@ -32,4 +29,42 @@ int main(int argc, char* argv[])
       printf("Unrecognized command '%s'.\n", input_buffer->buffer);
     }
   }
+}
+
+// function definition:
+
+InputBuffer* new_input_buffer()
+{
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void print_prompt()
+{ 
+    printf("db > "); 
+}
+
+void read_input(InputBuffer* input_buffer)
+{
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // Ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
 }
