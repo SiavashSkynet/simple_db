e1e897160ae1a0511031bffe838453e27903e9e2
siavash.shojaei <bro.shojaei@gmail.com>
2023-03-26 16:36:29 +0330

    #6 modify indents

--- cmd.c
+++ cmd.c
@@ -55,17 +55,17 @@ void print_prompt()
 
 void read_input(InputBuffer* input_buffer)
 {
-  ssize_t bytes_read =
-      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    ssize_t bytes_read =
+    getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-  if (bytes_read <= 0) {
-    printf("Error reading input\n");
-    exit(EXIT_FAILURE);
-  }
+    if (bytes_read <= 0) {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
 
-  // Ignore trailing newline
-  input_buffer->input_length = bytes_read - 1;
-  input_buffer->buffer[bytes_read - 1] = 0;
+    // Ignore trailing newline
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
 }
 
 void close_input_buffer(InputBuffer* input_buffer)
