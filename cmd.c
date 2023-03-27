403cbbb8c56dd6566355d14cff046013ce3f59da
siavash.shojaei <bro.shojaei@gmail.com>
2023-03-27 13:01:04 +0330

    #17 statements added

--- cmd.c
+++ cmd.c
@@ -32,18 +32,26 @@ void execute_statement(Statement* statement);
 
 int main(int argc, char* argv[])
 {
+    Statement* statement_command = (Statement*)malloc(sizeof(Statement));
     InputBuffer* input_buffer = new_input_buffer();
     while (true) {
         print_prompt();
         read_input(input_buffer);
-
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-        close_input_buffer(input_buffer);
-        exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if(input_buffer->buffer[0] == '.'){
+            if(!do_meta_command(input_buffer)){
+                close_input_buffer(input_buffer);
+                exit(EXIT_SUCCESS);
+            }
+            else{
+                printf("Wrong meta command\n");
+                continue;
+            }
+        }
+        if(!prepare_statement(input_buffer, statement_command)){
+            execute_statement(statement_command);
         }
     }
+    free(statement_command);
 }
 
 // function definition:
@@ -85,8 +93,10 @@ void read_input(InputBuffer* input_buffer)
 
 void close_input_buffer(InputBuffer* input_buffer)
 {    
-    if (input_buffer != NULL) {
-        free(input_buffer->buffer);
+   if (input_buffer != NULL) {
+        if (input_buffer->buffer != NULL) {
+            free(input_buffer->buffer);
+        }
         free(input_buffer);
     }
 }
